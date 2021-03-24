40eea7aefe1114e7109a243df62164be0435cb30
plam <plam@localhost.dev>
2021-03-24 10:04:21 +0000

    03_05 Add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
+import Nav from "./Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <Route path="/" component={Home} exact />
       <Route path="/profile" component={Profile} />
     </>
