3b924a62f3d49c219f0f386d01bdae167f3e381a
plam <plam@localhost.dev>
2021-03-24 11:02:20 +0000

    03_06 Style App03_06 Style App

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Nav from "./Nav";
 
 function App() {
   return (
-    <>
+    <div className="body">
       <Nav />
       <Route path="/" component={Home} exact />
       <Route path="/profile" component={Profile} />
-    </>
+    </div>
   );
 }
 
