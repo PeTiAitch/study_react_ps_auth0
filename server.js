451cb39f3e695941d60b77888ad46eafd46c3cf6
plam <plam@localhost.dev>
2021-03-29 11:49:04 +0000

    08_07 Validate Roles in Access Tokens

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ const checkJwt = jwt({
   algorithms: ["RS256"],
 });
 
+const checkRole = (role) => {
+  return function (req, res, next) {
+    const assignedRoles = req.user["http://localhost:3000/roles"];
+    if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+      return next();
+    } else {
+      return res.status(401).send("Insufficient role");
+    }
+  };
+};
+
 const app = express();
 
 app.get("/public", function (req, res) {
@@ -42,6 +53,12 @@ app.get("/course", checkJwt, checkScope(["read:courses"]), function (req, res) {
       { id: 1, title: "Building Apps with React and Redux" },
       { id: 2, title: "Creating Reusable React Components" },
     ],
+  });
+});
+
+app.get("/admin", checkJwt, checkRole("admin"), function (req, res) {
+  res.json({
+    message: "Hello from an admin API!",
   });
 });
 
